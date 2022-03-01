455e5520bcf6dd6701ab83d5618af44b73e88849
Kevin Babauta <kjr.babauta@gmail.com>
2022-03-01 09:28:19 -0800

    added some css, added db-atlas elements

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import Login from './Login'
-import { Route, Switch} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import CreateUser from './CreateUser'
+
+
+
 export default function App(){
     return(
         <div>
             <Login/>
-            <Switch>
+            <Routes>
                 <Route path="/" element={<Login/>}/>
                 <Route path="createUser" element={<CreateUser/>} />
-            </Switch>
+            </Routes>
             
         </div>
     )
